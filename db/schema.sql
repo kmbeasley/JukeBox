66f2905f864e812166f6656eb931b3643f7b5a22
Kyler Beasley <kylerbeasley7@gmail.com>
2026-06-05 12:04:02 -0500

    Added schema and seed code

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,25 @@
 -- TODO
+DROP TABLE IF EXISTS playlists_tracks;
+DROP TABLE IF EXISTS playlists;
+DROP TABLE IF EXISTS tracks;
+
+-- id, name, duration are NOT NULL, need to give back a value
+CREATE TABLE tracks (
+  id serial PRIMARY KEY,
+  name text NOT NULL,
+  duration_ms integer NOT NULL
+ );
+
+CREATE TABLE playlists (
+  id serial PRIMARY KEY,
+  name text NOT NULL,
+  description text NOT NULL
+);
+
+-- REFERENCES the other file it pulls the id from, and it will delete all files within since it is CASCADED. 
+CREATE TABLE playlists_tracks (
+  id serial PRIMARY KEY,
+  playlist_id integer NOT NULL REFERENCES playlists(id) ON DELETE CASCADE,
+  track_id integer NOT NULL REFERENCES tracks(id) ON DELETE CASCADE,
+  UNIQUE (playlist_id, track_id)
+);
